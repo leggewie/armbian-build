8bb0caf7921305824bf767ee956c4617710b8ee4
Rolf Leggewie <foss@leggewie.org>
2025-08-13 03:17:50 +0800

    WIP latest version of lib/tools/apt-install-first-level-recommends.py
    time to get some zzzZZZzzz now

--- lib/tools/apt-install-first-level-recommends.py
+++ lib/tools/apt-install-first-level-recommends.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
-import sys
-
-import apt
+import sys, os, apt, subprocess
 
 def auto_install(dependencies):
     for package in dependencies:
@@ -11,34 +9,51 @@ def auto_install(dependencies):
 
 recommends = False
 suggests = False
-if sys.argv[1] == "recommends":
-    recommends = True
-elif sys.argv[1] == "suggests":
-    suggests = True
-elif sys.argv[1] == "both":
-    recommends = True
-    suggests = True
-else:
-    print(f"Usage: {sys.argv[0]} [recommends|suggests|both] package1 package2 ...")
-    sys.exit(1)
 
-# Disable apt's automatic handling depending on what is specified
-if recommends:
-    apt.apt_pkg.config.set("APT::Install-Recommends", "false")
-if suggests:
-    apt.apt_pkg.config.set("APT::Install-Suggests", "false")
+try:
+    if os.geteuid() != 0:
+        raise PermissionError("The script must be run as root!")
+    # Check if at least two arguments are provided
+    if len(sys.argv) < 3:
+        raise IndexError("Insufficient arguments")
+    if sys.argv[1] in "recommends":
+        recommends = True
+    elif sys.argv[1] == "suggests":
+        suggests = True
+    elif sys.argv[1] == "both":
+        recommends = True
+        suggests = True
+    else:
+        raise ValueError("Please specify what additional packages should be installed!")
+except PermissionError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+except (IndexError, ValueError) as e:
+    print(f"Usage: {sys.argv[0]} [recommends|suggests|both] package1 package2 ...\n\n{e}")
+    sys.exit(2)
+except:
+    print(f"Unknown error")
+    sys.exit(3)
+
+# disable automatic installation of Suggests and Recommends
+apt.apt_pkg.config.set("APT::Install-Recommends", "false")
+apt.apt_pkg.config.set("APT::Install-Suggests", "false")
 
 cache = apt.Cache()
 
 for name in sys.argv[2:]:
-    package = cache[name]
-    package.mark_install()
-    candidate = package.candidate
-
-    if recommends:
-        auto_install(candidate.recommends)
-    if suggests:
-        auto_install(candidate.suggests)
+    # XXX handle error if package is not actually a package, no installation candidate
+    try:
+        package = cache[name]
+        package.mark_install()
+        candidate = package.candidate
+        if recommends:
+            auto_install(candidate.recommends)
+        if suggests:
+            auto_install(candidate.suggests)
+    except:
+        print(name + " has no installation candidate.")
+        sys.exit(4)
 
 changes = cache.get_changes()
 if not changes:
@@ -55,8 +70,15 @@ for change in sorted_changes:
     print(f"\t{change.name} ({', '.join(statuses)})")
 print(f"{len(sorted_changes)} packages will be installed, upgraded, or removed.")
 try:
-    input("Apply changes? (Press Enter to continue or Ctrl+C to cancel)")
-    cache.commit()
+    answer = input("Apply changes? (Press Enter to continue or Ctrl+C to cancel) ")
+    if answer.lower() in ["", "y", "yes"]:
+        cache.commit()
+    else:
+        print("\nOK, changes cancelled.")
+        sys.exit(1)
 except KeyboardInterrupt:
     print("\nChanges cancelled.")
     sys.exit(1)
+# the following does not work as expected
+except LockFailedException as e:
+    print("\nAnother process is managing the package cache at the moment.\n\n{e}")
