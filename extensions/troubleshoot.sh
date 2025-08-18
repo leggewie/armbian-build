f2dd3532019becc980ee0d753805d59bb3b93ba9
Rolf Leggewie <foss@leggewie.org>
2025-08-18 21:08:17 +0800

    Revert "WIP chroot_mount"
    This reverts commit ca3b5aab5726b847e822ff23664ba0ce9ceea2f4.

--- extensions/troubleshoot.sh
+++ extensions/troubleshoot.sh
@@ -6,6 +6,4 @@ function extension_prepare_config__apa() {
 
 function post_armbian_repo_customize_image__troubleshoot() {
 	chroot_sdcard find /var/cache/apt/
-	chroot_mount
-	chroot_sdcard find /var/cache/apt/
 }
